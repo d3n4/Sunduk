a4d5652ae846c78728f9728e0cb9b593d550e29f
d3n4 <id110099122@gmail.com>
2015-01-09 09:49:50 +0200

    Base implementation

--- Sunduk/API/SixPM.php
+++ Sunduk/API/SixPM.php
@@ -2,11 +2,18 @@
     namespace Sunduk\API {
         use Sunduk\Network\WebClient;
 
+        /**
+         * Class SixPM
+         * @todo getCategories() of SixPM_Category[] -> getProduct() of SixPM_Product
+         * @package Sunduk\API
+         */
         class SixPM {
             protected $webClient;
 
             public function __construct() {
                 $this->webClient = new WebClient();
+                $t = $this->webClient->get('http://www.6pm.com/goggles~2');
+                clog('response: {0}', $t);
             }
         }
     }
--- Sunduk/Network/WebClient.php
+++ Sunduk/Network/WebClient.php
@@ -1,42 +1,64 @@
 <?php
     namespace Sunduk\Network {
-        use Sunduk\IDisposable;
 
         /**
          * Class WebClient
-         * @package Sunduk\Network
          * @todo Cookie container
+         * @package Sunduk\Network
          */
         class WebClient {
 
             protected $_url;
-            protected $_isPost = false;
-            protected $_postFields = false;
-            protected $_isForm = false;
+            protected $_isPost;
+            protected $_fields;
+            protected $_isForm;
             protected $_lastResponse;
             protected $_error;
 
-            public function __construct(){
+            public function __construct() {
+            }
 
+            protected function reset() {
+                $this->_url = null;
+                $this->_fields = null;
+                $this->_isPost = false;
+                $this->_isForm = false;
+                $this->_lastResponse = null;
+                $this->_error = null;
+            }
+
+            public function get($url, $fields = null) {
+                $this->reset();
+                $this->_url = $url;
+                $this->_fields = $fields;
+                return $this->execute() ? $this->_lastResponse : null;
+            }
+
+            protected function formatGetFields($fields) {
+                if($fields == null)
+                    return '';
+                $format = '?';
+                foreach((array)$fields as $key => $value)
+                    $format .= $key . '=' . urlencode($value) . '&';
+                return $format;
             }
 
             /**
              * Execute current formatted request
              * @return bool Request success
              */
-            protected function Execute() {
-                $this->_error = null;
+            protected function execute() {
                 clog('[WebClient] Sending {0} Request to {1}', $this->_isPost ? 'POST' : 'GET', $this->_url);
                 if($curl = curl_init()) {
-                    curl_setopt($curl, CURLOPT_URL, $this->_url);
+                    curl_setopt($curl, CURLOPT_URL, $this->_url . (!$this->_isPost ? $this->formatGetFields($this->_fields) : ''));
                     if($this->_isForm) {
                         curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-type: multipart/form-data"));
                         curl_setopt($curl, CURLOPT_HEADER, true);
                     }
 
-                    if($this->_isPost === true){
+                    if($this->_isPost === true) {
                         curl_setopt($curl, CURLOPT_POST, true);
-                        curl_setopt($curl, CURLOPT_POSTFIELDS, $this->_postFields);
+                        curl_setopt($curl, CURLOPT_POSTFIELDS, $this->_fields);
                     }
 
                     curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
